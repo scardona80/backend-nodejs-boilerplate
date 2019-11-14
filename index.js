1001e92ab9c3f9089bbd934f91fcd5900f248916
Santiago Cardona <scardona@lean-tech.io>
2019-11-14 17:35:57 -0500

    Ticket #1 - First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const { config } = require('./config/index')
+
+const authApi = require('./routes/auth')
+const moviesApi = require('./routes/movies')
+const userMoviesApi = require('./routes/userMovies')
+
+const { 
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require('./utils/middlewares/errorHandlers')
+
+const notFoundHandler = require('./utils/middlewares/notFoundHandler')
+
+// Body parser
+app.use(express.json())
+
+// Routes
+authApi(app)
+moviesApi(app)
+userMoviesApi(app)
+
+// Catch 404 error
+app.use(notFoundHandler)
+
+// Error middlewares
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
+
+app.listen(config.port, () => {
+  console.log(`Listening on port http://localhost:${config.port}`)
+})
